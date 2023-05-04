173e53b6d425e8911b3783889abcd8e032eb1751
Knockgithub <131382142+Knockgithub@users.noreply.github.com>
2023-05-04 10:20:11 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,2 +1,2 @@
-welcome_text = 'Hi!\nWelcome to Optical Character Recognizer Bot.\nJust send a clear image to me and I will recognize the text in the image and send it as a message!\n\nIf you like my work and want me to continue working, consider donating any amount to keep this service alive.\nTo donate hit /donate\nEvery Contribution Counts!\n\nA part of @sjprojects'
+welcome_text = 'Hi!\nWelcome to Optical Character Recognizer Bot.\nJust send a clear image to me and I will recognize the text in the image and send it as a message!
 reply_to_text_message = 'Hey, I\'m not designed to reply to text messages 😕\nHit /start and read the instructions!'
